21eb7dbf393dc9e06282a7de2dade2654f0a0a99
Jakub Osika <osika.jakub@gmail.com>
2017-06-16 19:27:55 +0200

    Add entry point

--- src/main/java/greenhouse/Greenhouse.java
+++ src/main/java/greenhouse/Greenhouse.java
@@ -2,10 +2,44 @@ package greenhouse;
 
 import org.apache.log4j.Logger;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class Greenhouse {
-    final static Logger logger = Logger.getLogger(Greenhouse.class);
+    private final static Logger logger = Logger.getLogger(Greenhouse.class);
+    private String propertiesFileName;
 
     public static void main(String[] args) {
-        logger.info("Initial project");
+        logger.info("Greenhouse");
+        Greenhouse greenhouse = new Greenhouse("greenhouse.properties");
+        greenhouse.routine();
+    }
+
+    private Greenhouse(String propertiesFileName) {
+        this.propertiesFileName = propertiesFileName;
+    }
+
+    private Properties loadProperties(String filename) {
+        Properties prop = new Properties();
+        try {
+            InputStream input = this.getClass().getClassLoader().getResourceAsStream(filename);
+            if (input == null) {
+                logger.error("Unable to find properties " + filename);
+                System.exit(1);
+            }
+
+            prop.load(input);
+            input.close();
+
+        } catch (IOException ex) {
+            logger.error("Properties load error ", ex);
+            System.exit(1);
+        }
+        return prop;
+    }
+
+    private void routine() {
+        Properties properties = loadProperties(propertiesFileName);
     }
 }

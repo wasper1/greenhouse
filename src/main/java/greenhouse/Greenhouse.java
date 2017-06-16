e7cdd7291c5972b8c3daeee7082658aafd8b1ce3
Jakub Osika <osika.jakub@gmail.com>
2017-06-16 13:38:28 +0200

    Project init

---
+++ src/main/java/greenhouse/Greenhouse.java
@@ -0,0 +1,11 @@
+package greenhouse;
+
+import org.apache.log4j.Logger;
+
+public class Greenhouse {
+    final static Logger logger = Logger.getLogger(Greenhouse.class);
+
+    public static void main(String[] args) {
+        logger.info("Initial project");
+    }
+}

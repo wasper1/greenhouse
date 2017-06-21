7f94bf05edbcb0a2d3536b256b329f974334f774
Jakub Osika <osika.jakub@gmail.com>
2017-06-21 21:53:37 +0200

    Add sudo to start script

--- bin/start.sh
+++ bin/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 SCRIPT_DIR=`dirname "$0"`
-java -jar ${SCRIPT_DIR}/../target/greenhouse-1.0.0-jar-with-dependencies.jar ${SCRIPT_DIR}/greenhouse.properties
+sudo java -jar ${SCRIPT_DIR}/../target/greenhouse-1.0.0-jar-with-dependencies.jar ${SCRIPT_DIR}/greenhouse.properties
